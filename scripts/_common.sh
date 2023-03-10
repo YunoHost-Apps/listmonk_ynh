887e8fa60270591636141ad2dd944f60d6cdf88a
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2023-03-10 17:28:03 +0100

    v2

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,7 +5,7 @@
 #=================================================
 
 # dependencies used by the app
-pkg_dependencies="postgresql"
+#REMOVEME? pkg_dependencies="postgresql"
 
 nodejs_version=16
 GO_VERSION="1.18"
